a46bc9aae9d29a8c0883bfcae7fb8b357b9d46ba
Rajmund Staniek <rajmund.st@gmail.com>
2018-06-28 19:23:54 +0200

    implemented Caliburn.Micro MVVM framework into the client program

---
+++ test_assignment/test_assignment/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using test_assignment.ViewModels;
+
+namespace test_assignment
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
