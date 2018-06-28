e380d5a9ac7af5ce6126d607e475d80ab634f61e
Rajmund Staniek <rajmund.st@gmail.com>
2018-06-28 13:10:02 +0200

    covered tests for all API controllers

---
+++ test_assignment/API.Tests/Controllers/DistrictControllerTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using API.Controllers;
+using DatabaseLink.mapper;
+using DatabaseLink.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace API.Tests.Controllers
+{
+    [TestClass]
+    public class DistrictControllerTest
+    {
+
+        private DistrictController controller;
+
+        public DistrictControllerTest()
+        {
+            controller = new DistrictController();
+        }
+
+        [TestMethod]
+        public void DistrictControllerTest_GetAll()
+        {
+            var d = controller.Get();
+
+            Assert.AreNotEqual(0, d.ToList().Count());
+        }
+
+        [TestMethod]
+        public void DistrictControllerTest_Get()
+        {
+            var d = controller.Get(1);
+            string expected = "denmark";
+
+            Assert.IsTrue(d.Name.ToLower().Contains(expected));
+        }
+
+        [TestMethod]
+        public void DistrictControllerTest_POST_DELETE()
+        {
+            var d = new District()
+            {
+                Id = 0,
+                Name = "Norway",
+                PrimarySalesperson = new Salesperson()
+                {
+                    Id = 4
+                }
+            };
+
+            var countOld = controller.Get().ToList().Count;
+            controller.Post(d);
+            var countNew = controller.Get().ToList().Count;
+
+            Assert.AreNotEqual(countOld, countNew);
+
+            d = controller.Get().ToList().SingleOrDefault(x => { return (x.Name.Equals(d.Name) && x.PrimarySalesperson.Id.Equals(d.PrimarySalesperson.Id)); });
+            controller.Delete(d);
+            countNew = controller.Get().Count();
+
+            Assert.AreEqual(countOld, countNew);
+        }
+
+        [TestMethod]
+        public void DistrictControllerTest_Put()
+        {
+            var d = controller.Get(1);
+            string nameNew = "Norway";
+            string nameOld = d.Name;
+
+            d.Name = nameNew;
+            controller.Put(d.Id, d);
+            d = controller.Get(d.Id);
+
+            Assert.AreEqual(nameNew, d.Name);
+
+            d.Name = nameOld;
+            controller.Put(d.Id, d);
+        }
+
+        [TestMethod]
+        public void DistrictControllerTest_AssignSecondarySalesperson()
+        {
+            int person = 2;
+            int district = 4;
+
+            controller.AssignSecondaryToDistrict(district, person);
+        }
+
+        [TestMethod]
+        public void DistrictControllerTest_DeleteSecondarySalesperson()
+        {
+            int person = 2;
+            int district = 4;
+
+            controller.DeleteSecondaryFromDistrict(district, person);
+        }
+    }
+}
---
+++ test_assignment/API.Tests/Controllers/StoreControllerTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using API.Controllers;
+using DatabaseLink.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace API.Tests.Controllers
+{
+    [TestClass]
+    public class StoreControllerTest
+    {
+
+        private StoreController controller;
+
+        public StoreControllerTest()
+        {
+            controller = new StoreController();
+        }
+
+        [TestMethod]
+        public void StoreControllerTest_GetAll()
+        {
+            var stores = controller.Get();
+
+            Assert.AreNotEqual(0, stores.ToList().Count);
+        }
+
+        [TestMethod]
+        public void StoreControllerTest_Get()
+        {
+            var store = controller.Get(1);
+            var expected = "Shop 1";
+
+            Assert.AreEqual(expected, store.Name);
+        }
+
+        [TestMethod]
+        public void StoreControllerTest_POST_DELETE()
+        {
+            var store = new Store()
+            {
+                Id = 20,
+                Name = "Shop X",
+                Address = "Address X",
+                District = new District()
+                {
+                    Id = 2
+                }
+            };
+
+            var countOld = controller.Get().Count();
+            controller.Post(store);
+            var countNew = controller.Get().Count();
+
+            Assert.AreNotEqual(countOld, countNew);
+
+            store = controller.Get().ToList().SingleOrDefault(x => { return (x.Name.Equals(store.Name) && x.Address.Equals(store.Address)); });
+            controller.Delete(store);
+            countNew = controller.Get().Count();
+
+            Assert.AreEqual(countOld, countNew);
+        }
+
+        [TestMethod]
+        public void StoreControllerTest_Put()
+        {
+            var store = controller.Get(1);
+            string nameNew = "Shop X";
+            string nameOld = store.Name;
+
+            store.Name = nameNew;
+            controller.Put(store.Id, store);
+            store = controller.Get(store.Id);
+
+            Assert.AreEqual(nameNew, store.Name);
+            store.Name = nameOld;
+            controller.Put(store.Id, store);
+        }
+    }
+}
